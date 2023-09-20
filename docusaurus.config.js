719102fc8eb2c0fcd93c8bf9ef357ca927b12fab
sxyazi <sxyazi@gmail.com>
2023-09-20 09:06:49 +0800

    fix: github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
 							// },
 							{
 								label: "GitHub",
-								href : "https://github.com/facebook/docusaurus",
+								href : "https://github.com/sxyazi/yazi",
 							},
 						],
 					},
