0929941ca4f8a0dd1078f00159669e4b3fb31fe6
sxyazi <sxyazi@gmail.com>
2023-10-19 09:49:09 +0800

    feat: add 404 redirecting

---
+++ src/theme/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react"
+import NotFound from "@theme-original/NotFound"
+import { useEffect } from "react"
+
+export default function NotFoundWrapper(props) {
+	useEffect(() => {
+		if (location.href.includes("/docs/")) {
+			location.href = location.href.replace("/docs/", "/")
+		}
+	}, [])
+
+	return <NotFound {...props} />
+}
