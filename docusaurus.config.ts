be9ba1e406fbc07ac117f6c6b91a3093e509543e
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-01 05:39:11 -0400

    feat: typescript config file

---
+++ docusaurus.config.ts
@@ -0,0 +1,128 @@
+import type { Config } from "@docusaurus/types"
+import type * as Preset from "@docusaurus/preset-classic"
+
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+
+const config: Config = {
+	title: "Yazi",
+	tagline: "⚡️ Blazing fast terminal file manager written in Rust, based on async I/O.",
+	favicon: "img/logo.png",
+
+	url: "https://yazi-rs.github.io",
+	baseUrl: "/",
+
+	organizationName: "yazi-rs",
+	projectName: "yazi-rs.github.io",
+
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
+
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					path: "usage",
+					routeBasePath: "usage",
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/yazi-rs/yazi-rs.github.io/edit/main/",
+				},
+				blog: {
+					showReadingTime: true,
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/yazi-rs/yazi-rs.github.io/edit/main/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			} satisfies Preset.Options,
+		],
+	],
+
+	themeConfig: {
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "Yazi",
+			logo: {
+				alt: "Yazi file manager",
+				src: "img/logo.png",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "usageSidebar",
+					position: "left",
+					label: "Usage",
+				},
+				{ to: "/showcase", label: "Showcase", position: "left" },
+				{ to: "/blog", label: "Blog", position: "left" },
+				{
+					href: "https://github.com/sxyazi/yazi",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "Usage",
+							to: "/usage/installation",
+						},
+						{
+							label: "Showcase",
+							to: "/showcase",
+						},
+					],
+				},
+				{
+					title: "Community",
+					items: [
+						{
+							label: "Discord (English mainly)",
+							href: "https://discord.gg/qfADduSdJu",
+						},
+						{
+							label: "Telegram (Chinese mainly)",
+							href: "https://t.me/yazi_rs",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						// {
+						// 	label: "Blog",
+						// 	to: "/blog",
+						// },
+						{
+							label: "GitHub",
+							href: "https://github.com/sxyazi/yazi",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Yazi. Built with ❤️️.`,
+		},
+		prism: {
+			theme: lightCodeTheme,
+			darkTheme: darkCodeTheme,
+			additionalLanguages: ["lua", "toml"],
+		},
+	} satisfies Preset.ThemeConfig,
+}
+
+export default config
