6c63add623298872d905479423e521c71758469f
sxyazi <sxyazi@gmail.com>
2023-11-06 23:24:51 +0800

    docs: add a note about the built-in feature showcase being moved to a separate page

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -53,7 +53,22 @@ export function Cards({ from: cards }: { from: CardProps[] }) {
 		<section className="margin-top--lg margin-bottom--xl">
 			<div className="container">
 				<div className={clsx("margin-bottom--md")}>
-					<Heading as="h2">Our favorites ✨</Heading>
+					{ cards.length ? <Heading as="h2">Our favorites ✨</Heading> : <></> }
+
+					{ /* TODO: remove this once the plugin system is ready */ }
+					{ !cards.length && <>
+						<br/><br/>
+						<Heading as="h3" style={{ color: "blue" }}>
+							The plugin system is coming soon, stay tuned!
+						</Heading>
+
+						<br/>
+						<Heading as="h3" style={{ color: "red" }}>
+							The built-in feature showcase, has been moved to <a href="/features" style={{ textDecoration: "underline" }}>a separate "Features" page</a>.
+						</Heading>
+					</> }
+					{ /* TODO: remove this once the plugin system is ready */ }
+
 				</div>
 				<ul className={clsx("container", "clean-list", styles.cards)}>
 					{cards.map(card => Card(card))}
