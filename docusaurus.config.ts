a84de444c57de1a0a69bfa3831594a3106453249
Lintao <lintao.amons@outlook.com>
2024-01-16 12:09:49 +0800

    feat: add search function

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,6 +44,23 @@ const config: Config = {
 		],
 	],
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
+
 	themeConfig: {
 		// Replace with your project's social card
 		image: "img/docusaurus-social-card.jpg",
